f196c16a68c342a0f73c1486ceedbde61c51205b
Ignacio Calvagna <ignaciocalvagna@gmail.com>
2022-11-22 17:02:47 -0300

    .

--- proyectoFinal/primerEntregaProyectoFinal/routes/products.js
+++ proyectoFinal/primerEntregaProyectoFinal/routes/products.js
@@ -4,18 +4,7 @@ const productController = require("../controllers/productsController");
 const Contenedor = require("../Contenedor");
 const Producto = new Contenedor("./products.json");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const productos = await Producto.getAll();
-    if (!productos[0]) {
-        res.json("no hay productos");
-    }
-    
-    res.json(productos);
-  } catch (e) {
-    next(e);
-  }
-});
+router.get("/", productController.getAll);
 router.get("/:id", productController.getById);
 router.post("/", productController.addProduct);
 router.put("/:id", productController.edit);
